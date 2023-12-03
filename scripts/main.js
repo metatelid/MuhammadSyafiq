b95649e0e294585d844695413bb87b7d6d3652d9
metatelid <88137714+metatelid@users.noreply.github.com>
2023-12-03 21:32:26 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,140 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function() {
+  AOS.init({
+    // uncomment below for on-scroll animations to played only once
+    // once: true
+  }); // initialize animate on scroll library
+});
+
+$(document).ready(function() {
+    $('.btn-modal').trigger('click');
+});
+
+const play = document.querySelector('.play')
+const pause = document.querySelector('.pause')
+
+var el = document.getElementById("music");
+function playAudio() { 
+  var inputData = document.getElementById("inputData").value;
+  if (inputData.trim() === "") {
+    alert("Tolong Isi Nama Lengkap Anda Terlebih Dahulu!");
+  } else {
+    $('#exampleModal').modal('hide');
+    el.play();
+  }
+  pause.classList.remove('d-none');
+  play.classList.add('d-none');
+} 
+
+// Smooth scroll for links with hashes
+$("a.smooth-scroll").click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000,
+        function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        }
+      );
+    }
+  }
+});
+
+// Photo Filter
+var activeFilter = "all";
+
+$(".ww-filter-button").on("click", function(e) {
+  // remove btn-primary from all buttons first
+  $(".ww-filter-button").removeClass("btn-primary");
+  $(".ww-filter-button").addClass("btn-outline-primary");
+
+  // add btn-primary to active button
+  var button = $(this);
+  button.removeClass("btn-outline-primary");
+  button.addClass("btn-primary");
+  filterItems(button.data("filter"));
+  e.preventDefault();
+});
+
+function filterItems(filter) {
+  if (filter === activeFilter) {
+    return;
+  }
+
+  activeFilter = filter;
+  $(".ww-gallery .card").each(function() {
+    var card = $(this);
+    var groups = card.data("groups");
+    var show = false;
+    if (filter === "all") {
+      show = true;
+    } else {
+      for (var i = 0; i < groups.length; i++) {
+        if (groups[i] === filter) {
+          show = true;
+        }
+      }
+    }
+    // hide everything first
+    card.fadeOut(400);
+    setTimeout(function() {
+      if (show && !card.is(":visible")) {
+        card.fadeIn(400);
+      }
+    }, 500);
+  });
+}
+
+// Light Box
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+function copy_mandiri() {
+    document.getElementById("mandiri").select();
+    document.execCommand("copy");
+    alert('No. Rekening Berhasil di Copy, Kami tunggu tranferannya :)');
+}
+
+// Mengambil elemen inputan
+var inputData = document.getElementById("inputData");
+
+// Mengambil elemen hasil
+var hasilInputan = document.getElementById("hasilInputan");
+
+// Fungsi untuk menangani input dari pengguna
+function tampilkanInput() {
+  // Mengambil nilai dari inputan
+  var nilaiInput = inputData.value;
+
+  // Menampilkan nilai inputan di elemen hasil
+  hasilInputan.textContent = "Kepada Yth. " + nilaiInput;
+}
+
+// Menambahkan event listener untuk memanggil fungsi tampilkanInput() saat input berubah
+inputData.addEventListener("input", tampilkanInput);
